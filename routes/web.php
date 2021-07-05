173d49bfa413ed21b1b23ef1c6acde76495b04d0
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2021-07-05 15:47:08 +0600

    Crud Operation For LocDivision.php

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,14 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 $router->get('/hello', 'ExampleController@hateoasResponse');
 
-$router->group(['prefix'=>'api/v1'], function() use($router){
+$router->group( ['prefix'=>'api/v1'], function() use($router){
+    /* division Crud Operation*/
     $router->get('/division', 'LocDivisionController@index');
-    $router->post('/division', 'LocDivisionController@store');
-    $router->get('/division/{id}', 'LocDivisionController@show');
-    $router->put('/division/{id}', 'LocDivisionController@update');
-    $router->delete('/division/{id}', 'LocDivisionController@destroy');
+    $router->post('/division/add', 'LocDivisionController@store');
+    $router->get('/division/show/{id}', 'LocDivisionController@show');
+    $router->get('/division/edit/{id}', 'LocDivisionController@show');
+    $router->put('/division/update/{id}', 'LocDivisionController@update');
+    $router->delete('/division/delete/{id}', 'LocDivisionController@destroy');
+
+
 });
