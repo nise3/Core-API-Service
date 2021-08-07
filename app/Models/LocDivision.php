4f9eb6af337d6b15f5d8116e6d320e0053563dd7
Miladul Islam <imiladul@gmail.com>
2021-08-07 11:01:05 +0600

    Upazila API service fixed

--- app/Models/LocDivision.php
+++ app/Models/LocDivision.php
@@ -3,10 +3,10 @@
 namespace App\Models;
 
 use App\Traits\Scopes\ScopeRowStatusTrait;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Date;
 
 /**
  * Class LocDivision
