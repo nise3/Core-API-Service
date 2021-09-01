fa22baf0ed9289cbbd4d2d49cf9104611f3e17d0
Md Tasmidur Rahman <tasmidurrahman@gmail.com>
2021-09-01 13:47:34 +0600

    add content management

---
+++ database/migrations/2021_04_27_110524_create_video_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('institute_id');
+            $table->string('title_en', 191);
+            $table->string('title_bn', 191);
+            $table->unsignedInteger('parent_id')->nullable();
+            $table->unsignedTinyInteger('row_status')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_categories');
+    }
+}
