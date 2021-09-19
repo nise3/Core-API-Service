08c7b2b6fc0457666d7c0e8436b5078c2c6253c0
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-09-19 11:10:42 +0600

    Fixing the scope Directory Name

--- app/Http/Controllers/ApiInfoController.php
+++ app/Http/Controllers/ApiInfoController.php
@@ -3,39 +3,39 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Response;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
 
 class ApiInfoController extends Controller
 {
-    const SERVICE_NAME='NISE-3 Core Api Service';
-    const SERVICE_VERSION='V1';
+    const SERVICE_NAME = 'NISE-3 Core Api Service';
+    const SERVICE_VERSION = 'V1';
+
     public function apiInfo(): JsonResponse
     {
-        $response=[
-            'service_name'=>self::SERVICE_NAME,
-            'service_version'=>self::SERVICE_VERSION,
-            'lumen_version'=>App::version(),
-            'modules'=>[
-                'LocationManagement'=>[
+        $response = [
+            'service_name' => self::SERVICE_NAME,
+            'service_version' => self::SERVICE_VERSION,
+            'lumen_version' => App::version(),
+            'modules' => [
+                'LocationManagement' => [
                     'LocDivision',
                     'LocDistrict',
                     'LocUpazila'
                 ],
-                'UserRoleManagement'=>[
+                'UserRoleManagement' => [
                     'Permission',
                     'Role',
                     'User',
                     'PermissionGroup',
                     'PermissionSubGroup'
                 ],
-                'AuthManagement'=>[
+                'AuthManagement' => [
                     'Auth'
                 ]
             ],
-            'description'=> 'It a core api service that manages Location Services,UserRoleManagement Services and Auth Services globally'
+            'description' => 'It a core api service that manages Location Services,UserRoleManagement Services and Auth Services globally'
 
         ];
         return Response::json($response, ResponseAlias::HTTP_OK);
