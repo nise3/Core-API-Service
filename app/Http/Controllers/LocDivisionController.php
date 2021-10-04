2aad3b0f5883e43ec4f375a3bc15f606342ec90a
rrahul <rahulbgc21@gmail.com>
2021-10-04 13:38:13 +0600

    refactor

--- app/Http/Controllers/LocDivisionController.php
+++ app/Http/Controllers/LocDivisionController.php
@@ -9,7 +9,6 @@ use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
