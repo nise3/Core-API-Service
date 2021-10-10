5b6931379067cd4f07d4c458e4ca78933d6b0676
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2021-10-10 19:16:44 +0600

    helper file update to fix the correct service host name

--- app/Helpers/functions/helpers.php
+++ app/Helpers/functions/helpers.php
@@ -1,44 +1,50 @@
 <?php
 
+use Illuminate\Support\Carbon;
+
 if (!function_exists("clientUrl")) {
     function clientUrl($type)
     {
-        $config = config("httpclientendpoint");
-
         if (!in_array(request()->getHost(), ['localhost', '127.0.0.1'])) {
             if ($type == "CORE") {
-                return env("IS_DEVELOPMENT_MOOD",false) ? $config["core"]["dev"] : $config["core"]["prod"];
+                return config("nise3.is_dev_mode") ? config("httpclientendpoint.core.dev") : config("httpclientendpoint.core.prod");
             } elseif ($type == "ORGANIZATION") {
-                return env("IS_DEVELOPMENT_MOOD",false) ? $config["organization"]["dev"] : $config["organization"]["prod"];
+                return config("nise3.is_dev_mode") ? config("httpclientendpoint.organization.dev") : config("httpclientendpoint.organization.prod");
+//                return config("nise3.is_dev_mode") ? $config["organization"]["dev"] : $config["organization"]["prod"];
             } elseif ($type == "INSTITUTE") {
-                return env("IS_DEVELOPMENT_MOOD",false) ? $config["institute"]["dev"] : $config["institute"]["prod"];
+                return config("nise3.is_dev_mode") ? config("httpclientendpoint.institute.dev") : config("httpclientendpoint.institute.prod");
+//                return config("nise3.is_dev_mode") ? $config["institute"]["dev"] : $config["institute"]["prod"];
             } elseif ($type == "IDP_SERVER") {
-                return env("IS_DEVELOPMENT_MOOD",false) ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
+                return config("nise3.is_dev_mode") ? config("httpclientendpoint.idp_server.dev") : config("httpclientendpoint.idp_server.prod");
+//                return config("nise3.is_dev_mode") ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
             }
 
         } else {
             if ($type == "CORE") {
-                return $config["core"]["local"];
+                return config("httpclientendpoint.core.local");
+//                return $config["core"]["local"];
             } elseif ($type == "ORGANIZATION") {
-                return $config["organization"]["local"];
+                return config("httpclientendpoint.organization.local");
+//                return $config["organization"]["local"];
             } elseif ($type == "INSTITUTE") {
-                return $config["institute"]["local"];
+                return config("httpclientendpoint.institute.local");
+//                return $config["institute"]["local"];
             } elseif ($type == "IDP_SERVER") {
-                return env("IS_DEVELOPMENT_MOOD") ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
+                config("nise3.is_dev_mode") ? config("httpclientendpoint.idp_server.dev") : config("httpclientendpoint.idp_server.prod");
+//                return config("nise3.is_dev_mode") ? $config["idp_server"]["dev"] : $config["idp_server"]["prod"];
             }
         }
         return "";
     }
 }
-
 if (!function_exists('formatApiResponse')) {
     /**
-     * @param $path
-     * @param bool $secured
-     *
+     * @param $data
+     * @param $startTime
+     * @param int $statusCode
      * @return array
      */
-    function formatApiResponse($data, $startTime, $statusCode = 200): array
+    function formatApiResponse($data, $startTime, int $statusCode = 200): array
     {
         return [
             "data" => $data ?: null,
