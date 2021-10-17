1110c672a9afff0c4aaeba1f31c81c6031533e81
rrahul <rahulbgc21@gmail.com>
2021-10-17 13:21:19 +0600

    auth service provider code refactoring

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Helpers\Classes\HttpClientRequest;
+use App\Models\Role;
+use App\Models\User;
 use App\Services\UserRolePermissionManagementServices\UserService;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
@@ -28,21 +31,52 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->app['auth']->viaRequest('token', function ($request) {
             $token = $request->header('Authorization');
+            Log::info($token);
             $authUser = null;
             if ($token) {
-                $header = explode(" ", $token);
-                if (count($header) > 1) {
-                    $tokenParts = explode(".", $header[1]);
-                    if (count($tokenParts) == 3) {
-                        $tokenPayload = base64_decode($tokenParts[1]);
-                        $jwtPayload = json_decode($tokenPayload);
-                        $userService = $this->app->make(UserService::class);
-                        $authUser = $userService->getAuthPermission($jwtPayload->sub ?? null);
-                    }
+                //$header = explode(" ", $token);
+                $token = trim(str_replace('Bearer', '', $token));
+
+                $idpServerId = $this->getIdpServerIdFromToken($token);
+                Log::info("Auth idp user id-->" . $idpServerId);
+                if($idpServerId){
+                    $userService = $this->app->make(UserService::class);
+                    $authUser = $userService->getAuthPermission($idpServerId);
+                    Log::info("userInfoWithIdpId:" . json_encode($authUser));
+                    return $authUser;
                 }
-                Log::info("userInfoWithIdpId:" . json_encode($authUser));
             }
-            return $authUser;
+
+
+//            $token = $request->header('Authorization');
+//            $authUser = null;
+//            if ($token) {
+//                $header = explode(" ", $token);
+//                if (count($header) > 1) {
+//                    $tokenParts = explode(".", $header[1]);
+//                    if (count($tokenParts) == 3) {
+//                        $tokenPayload = base64_decode($tokenParts[1]);
+//                        $jwtPayload = json_decode($tokenPayload);
+//                        $userService = $this->app->make(UserService::class);
+//                        $authUser = $userService->getAuthPermission($jwtPayload->sub ?? null);
+//                    }
+//                }
+//                Log::info("userInfoWithIdpId:" . json_encode($authUser));
+//            }
+//            return $authUser;
         });
+    }
+
+    private function getIdpServerIdFromToken($data, $verify = false)
+    {
+        $sections = explode('.', $data);
+        if (count($sections) < 3) {
+            throw new \Exception('Invalid number of sections of Tokens (<3)');
+        }
+
+        list($header, $claims, $signature) = $sections;
+        preg_match("/['\"]sub['\"]:['\"](.*?)['\"][,]/", base64_decode($claims), $matches);
+
+        return count($matches) > 1 ? $matches[1] : "";
     }
 }
