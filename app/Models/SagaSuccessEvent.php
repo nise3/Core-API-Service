d7c8f8ddaf3258eb62c4c44c408ee988c7d45f4d
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2021-12-09 01:24:43 +0600

    Added all Configuration for Rabbit + SAGA

--- app/Models/SagaSuccessEvent.php
+++ app/Models/SagaSuccessEvent.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class SagaSuccessEvent extends Model
+class SagaSuccessEvent extends BaseModel
 {
     protected $guarded = BaseModel::COMMON_GUARDED_FIELDS_SIMPLE;
 
