f53c52be74b40e3e7d07a1f7672816a1bcee388a
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2022-01-01 17:26:53 +0600

    Route file updated to serve new sso config

--- routes/web.php
+++ routes/web.php
@@ -56,12 +56,13 @@ $router->post('/sso-authorize-code-grant', function (\Illuminate\Http\Request $r
             throw new Symfony\Component\HttpKernel\Exception\HttpException(\Symfony\Component\HttpFoundation\Response::HTTP_FORBIDDEN);
         }*/
 
-    $basicAuth = 'Basic ' . base64_encode('FhVqwNp6Q6FV1H8KuuLsh5REQysa:GfrDpy904LjaWNmn7aSwEA1qyEQa');
-    $postUrl = 'https://bus-staging.softbdltd.com/oauth2/token?grant_type=authorization_code&code=' . $request->input('code') . '&redirect_uri=' . urlencode($request->input('redirect_uri'));
-    if (config("nise3.is_dev_mode")) {
+    $basicAuth = 'Basic ' . base64_encode(env('WSO2_IDP_CLIENT_KEY', 'FhVqwNp6Q6FV1H8KuuLsh5REQysa'). ':'. env('WSO2_IDP_CLIENT_SECRET', 'GfrDpy904LjaWNmn7aSwEA1qyEQa'));
+    $postUrl = env('WSO2_IDP_BASE_URL', 'https://bus-staging.softbdltd.com/') . 'oauth2/token?grant_type=authorization_code&code=' . $request->input('code') . '&redirect_uri=' . urlencode($request->input('redirect_uri'));
+
+    /*    if (config("nise3.is_dev_mode")) {
         $postUrl = 'https://192.168.13.206:9448/oauth2/token?grant_type=authorization_code&code=' . $request->input('code') . '&redirect_uri=' . urlencode($request->input('redirect_uri'));
         $basicAuth = 'Basic ' . base64_encode('FoYnKgvzdAwUGgyfDjj4torgkYYa:pASKCuZCu_w7erAsWNkCZHMHMv8a');
-    }
+    }*/
 
     try {
         $responseData = \Illuminate\Support\Facades\Http::withHeaders([
@@ -103,12 +104,9 @@ $router->post('/sso-renew-access-token', function (\Illuminate\Http\Request $req
 
     \Illuminate\Support\Facades\Log::debug('refresh token: ' . $request->input('refresh_token'));
 
-    $basicAuth = 'Basic ' . base64_encode('FhVqwNp6Q6FV1H8KuuLsh5REQysa:GfrDpy904LjaWNmn7aSwEA1qyEQa');
-    $postUrl = "https://bus-staging.softbdltd.com/oauth2/token?grant_type=refresh_token&refresh_token=" . $request->input('refresh_token');
-    if (config("nise3.is_dev_mode")) {
-        $postUrl = "https://192.168.13.206:9448/oauth2/token?grant_type=refresh_token&refresh_token=" . $request->input('refresh_token');
-        $basicAuth = 'Basic ' . base64_encode('FoYnKgvzdAwUGgyfDjj4torgkYYa:pASKCuZCu_w7erAsWNkCZHMHMv8a');
-    }
+    $basicAuth = 'Basic ' . base64_encode(env('WSO2_IDP_CLIENT_KEY', 'FhVqwNp6Q6FV1H8KuuLsh5REQysa'). ':'. env('WSO2_IDP_CLIENT_SECRET', 'GfrDpy904LjaWNmn7aSwEA1qyEQa'));
+
+    $postUrl = env('WSO2_IDP_BASE_URL', 'https://bus-staging.softbdltd.com/') . "oauth2/token?grant_type=refresh_token&refresh_token=" . $request->input('refresh_token');
 
     try {
         $responseData = \Illuminate\Support\Facades\Http::withHeaders([
