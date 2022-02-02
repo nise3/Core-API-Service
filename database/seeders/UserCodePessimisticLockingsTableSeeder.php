d89bc45641b342319a79715be03626497112ec0d
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-02 10:54:04 +0600

    version: 2.0.36 adding UserCodePessimisticLockingsTableSeeder.php file

---
+++ database/seeders/UserCodePessimisticLockingsTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class UserCodePessimisticLockingsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        Schema::disableForeignKeyConstraints();
+
+        DB::table('user_code_pessimistic_lockings')->truncate();
+
+        DB::table('user_code_pessimistic_lockings')->insert(array(
+            0 =>
+                array(
+                    'last_incremental_value' => 5,
+                ),
+        ));
+
+        Schema::enableForeignKeyConstraints();
+
+
+    }
+}
