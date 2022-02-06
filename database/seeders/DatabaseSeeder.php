0aca8de4920cd204a63e57cf3073eac4b083388a
A.S.M. Mahmudul Hasan <asmmahmud@gmail.com>
2022-02-06 21:24:21 +0600

    version: 2.1.4

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Cache::flush();
         $this->call([
             DomainsTableSeeder::class,
             GeoLocationDatabaseSeeder::class,
