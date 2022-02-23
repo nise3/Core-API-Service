4909fede1e5c409c3a90c7f381b57c06ece1bd1d
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-23 12:43:54 +0600

    add trainer model change

--- database/migrations/2022_02_23_054326_add_columns_to_users_table.php
+++ database/migrations/2022_02_23_054326_add_columns_to_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTrainerIdToUsersTable extends Migration
+class AddColumnsToUsersTable extends Migration
 {
     /**
      * Run the migrations.
