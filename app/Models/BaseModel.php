3e4fe961186231735fb82937191dd4654cad61a0
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-23 17:03:32 +0600

    added trainer & tro in basemodel

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -61,13 +61,17 @@ abstract class BaseModel extends Model
     public const INSTITUTE_USER = 3;
     public const YOUTH_USER_TYPE = 4;
     public const INDUSTRY_ASSOCIATION_USER = 5;
+    public const TRAINER_USER = 6;
+    public const REGISTERED_TRAINING_ORGANIZATION_USER = 7;
 
     /**User Types*/
     public const USER_TYPES = [
         self::SYSTEM_USER,
         self::ORGANIZATION_USER,
         self::INSTITUTE_USER,
-        self::INDUSTRY_ASSOCIATION_USER
+        self::INDUSTRY_ASSOCIATION_USER,
+        self::TRAINER_USER,
+        self::REGISTERED_TRAINING_ORGANIZATION_USER
     ];
 
     public const USER_TYPE = [
@@ -75,6 +79,8 @@ abstract class BaseModel extends Model
         self::ORGANIZATION_USER => 'organization',
         self::INSTITUTE_USER => 'institute',
         self::INDUSTRY_ASSOCIATION_USER => 'industry-association',
+        self::TRAINER_USER => 'trainer',
+        self::REGISTERED_TRAINING_ORGANIZATION_USER => 'registered-training-organization'
     ];
     public const USER_CODE_SIZE = 11;
     public const USER_CODE_PREFIXES = [
@@ -82,6 +88,8 @@ abstract class BaseModel extends Model
         self::ORGANIZATION_USER => 'UIND',
         self::INSTITUTE_USER => 'USSP',
         self::INDUSTRY_ASSOCIATION_USER => 'UINA',
+        self::TRAINER_USER => 'UTRA',
+        self::REGISTERED_TRAINING_ORGANIZATION_USER => 'URTO'
     ];
 
     public const MOBILE_REGEX = 'regex: /^(01[3-9]\d{8})$/';
