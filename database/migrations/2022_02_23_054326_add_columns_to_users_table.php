f2962b20f7d4f0134c9b22c89413f2aabf352ae2
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-02-23 12:41:49 +0600

    add trainer model change

---
+++ database/migrations/2022_02_23_054326_add_columns_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTrainerIdToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedTinyInteger('user_type')->comment('1 => system user, 2 => organization user, 3 => institute user, 4 => youth user, 5 => industry association user, 6 => trainer user, 7 => rto user')->change();
+            $table->unsignedInteger('trainer_id')->nullable()->after('industry_association_id');
+            $table->unsignedInteger('rto_id')->nullable()->after('trainer_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('trainer_id');
+            $table->dropColumn('rto_id');
+        });
+    }
+}
