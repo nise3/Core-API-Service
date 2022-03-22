d1341cccb48efc41e90460bbea41a63ab19f00ae
Md Anisur Rahman <anisur.rahman@softbdltd.com>
2022-03-22 12:38:22 +0600

    RELEASE = 'php artisan list'

--- database/seeders/DomainsTableSeeder.php
+++ database/seeders/DomainsTableSeeder.php
@@ -67,6 +67,10 @@ class DomainsTableSeeder extends Seeder
             array('domain' => 'rpl-dev.nise3.xyz', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => NULL,'title_prefix'=>'RPL-DEV' , 'title_prefix_en'=>'RPL-DEV', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
             array('domain' => 'rpl.nise.asm', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => NULL,'title_prefix'=>'RPL-ASM' , 'title_prefix_en'=>'RPL-ASM', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
             array('domain' => 'ssp-training.nise3.xyz', 'institute_id' => 47, 'organization_id' => NULL, 'industry_association_id' => NULL,'title_prefix'=>'SSP-Training' , 'title_prefix_en'=>'SSP-Training', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
+            array('domain' => 'bgapmea.nise.gov.bd', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => 28,'title_prefix'=>'BGAPMEA' , 'title_prefix_en'=>'BGAPMEA', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
+            array('domain' => 'beioa.nise.gov.bd', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => 29,'title_prefix'=>'BEIOA' , 'title_prefix_en'=>'BEIOA', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
+            array('domain' => 'bcmea.nise.gov.bd', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => 33,'title_prefix'=>'BCMEA' , 'title_prefix_en'=>'BCMEA', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
+            array('domain' => 'bfea.nise.gov.bd', 'institute_id' => NULL, 'organization_id' => NULL, 'industry_association_id' => 36,'title_prefix'=>'BFEA' , 'title_prefix_en'=>'BFEA', 'created_at' => '2022-01-30 18:00:34', 'updated_at' => '2022-01-30 18:00:34'),
         ]);
 
         Schema::enableForeignKeyConstraints();
