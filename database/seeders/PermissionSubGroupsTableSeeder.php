dadfd3e85b9eebb6f6931560e88e5547a92423d5
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-03-24 18:28:12 +0600

    Permission related seeder file updated

--- database/seeders/PermissionSubGroupsTableSeeder.php
+++ database/seeders/PermissionSubGroupsTableSeeder.php
@@ -33,8 +33,8 @@ class PermissionSubGroupsTableSeeder extends Seeder
             array('id' => '8', 'permission_group_id' => '4', 'title_en' => 'Industry Permission Sub Group - 2', 'title' => 'শিল্প প্রতিষ্ঠান পারমিশন সাব গ্রুপ - ২', 'key' => 'industry_permission_sub_group_2', 'row_status' => '1', 'created_at' => '2022-01-05 16:53:45', 'updated_at' => '2022-01-05 16:53:45'),
             array('id' => '9', 'permission_group_id' => '6', 'title_en' => 'Registered Training Organization Sub Group -1', 'title' => 'নিবন্ধিত প্রশিক্ষণ সংস্থা সাব গ্রুপ - ১', 'key' => 'registered_training_organization_sub_group_1', 'row_status' => '1', 'created_at' => '2022-03-06 23:05:57', 'updated_at' => '2022-03-06 23:06:32'),
             array('id' => '10', 'permission_group_id' => '6', 'title_en' => 'Registered Training Organization Sub Group - 2', 'title' => 'নিবন্ধিত প্রশিক্ষণ সংস্থা সাব গ্রুপ - ২', 'key' => 'registered_training_organization_sub_group_2', 'row_status' => '1', 'created_at' => '2022-03-06 23:07:26', 'updated_at' => '2022-03-06 23:07:26'),
-            array('id' => '11', 'permission_group_id' => '7', 'title_en' => 'Certificate Authority Permission sub group - 1', 'title' => 'সার্টিফিকেট অথোরিটি পারমিশন সাব গ্রুপ - ১', 'key' => 'certificate_authority_sub_group', 'row_status' => '1', 'created_at' => '2022-03-09 17:58:27', 'updated_at' => '2022-03-09 23:39:12')
-
+            array('id' => '11', 'permission_group_id' => '7', 'title_en' => 'Certificate Authority Permission sub group - 1', 'title' => 'সার্টিফিকেট অথোরিটি পারমিশন সাব গ্রুপ - ১', 'key' => 'certificate_authority_sub_group', 'row_status' => '1', 'created_at' => '2022-03-09 17:58:27', 'updated_at' => '2022-03-09 23:39:12'),
+            array('id' => '12', 'permission_group_id' => '2', 'title_en' => 'Skill/Training Provider Permission Sub Group (DSS) -3', 'title' => 'দক্ষতা/প্রশিক্ষণ প্রদানকারী পারমিশন সাব গ্রুপ (ডিএসএস) -৩', 'key' => 'ssp_permission_sub_group_3_dss', 'row_status' => '1', 'created_at' => '2022-03-24 20:55:40', 'updated_at' => '2022-03-24 20:55:40')
         ]);
 
         Schema::enableForeignKeyConstraints();
