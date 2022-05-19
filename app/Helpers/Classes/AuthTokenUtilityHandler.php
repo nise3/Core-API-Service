d9644af1f4686c3dd9a830cf11403f383771fdeb
ASM Mahmudul Hasan <asmmahmud@gmail.com>
2022-05-19 16:19:05 +0600

    Coding Draft for User Token Validation functionality

--- app/Helpers/Classes/AuthTokenUtilityHandler.php
+++ app/Helpers/Classes/AuthTokenUtilityHandler.php
@@ -7,6 +7,8 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
 use Throwable;
+use Firebase\JWT\CachedKeySet;
+use Firebase\JWT\JWT;
 
 class AuthTokenUtilityHandler
 {
@@ -44,10 +46,12 @@ class AuthTokenUtilityHandler
     {
         return self::_WSO2_KEY;
     }
+
     private function verify($token): bool
     {
         return true;
     }
+
     /**
      * Verify Signature
      * @throws \Illuminate\Http\Client\RequestException
@@ -154,5 +158,34 @@ class AuthTokenUtilityHandler
         $json_without_bigints = preg_replace('/:\s*(-?\d{' . $max_int_length . ',})/', ': "$1"', $input);
 
         return json_decode($json_without_bigints);
+    }
+
+    public function validateJwtUserTokenRaw($token)
+    {
+
+//
+//// The URI for the JWKS you wish to cache the results from
+//        $jwksUri = 'https://www.gstatic.com/iap/verify/public_key-jwk';
+//
+//// Create an HTTP client (can be any PSR-7 compatible HTTP client)
+//        $httpClient = new GuzzleHttp\Client();
+//
+//// Create an HTTP request factory (can be any PSR-17 compatible HTTP request factory)
+//        $httpFactory = new GuzzleHttp\Psr\HttpFactory();
+//
+//// Create a cache item pool (can be any PSR-6 compatible cache item pool)
+//        $cacheItemPool = Phpfastcache\CacheManager::getInstance('files');
+//
+//        $keySet = new CachedKeySet(
+//            $jwksUri,
+//            $httpClient,
+//            $httpFactory,
+//            $cacheItemPool,
+//            null, // $expiresAfter int seconds to set the JWKS to expire
+//            true  // $rateLimit    true to enable rate limit of 10 RPS on lookup of invalid keys
+//        );
+//
+//        $jwt = 'eyJhbGci...'; // Some JWT signed by a key from the $jwkUri above
+//        $decoded = JWT::decode($jwt, $keySet);
     }
 }
